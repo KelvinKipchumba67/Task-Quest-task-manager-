0ce99e88c9edcaa9163e277fbf27d2de54f1a0f1
Kelvin <kelvinkipchumba935@gmail.com>
2025-09-21 23:26:56 +0300

    Fixing the enviroment variables

---
+++ config/env.prod.js
@@ -0,0 +1,10 @@
+// config/env.prod.js - Safe for production (no secrets exposed)
+window.ENV_PROD = {
+  FIREBASE_API_KEY: "AIzaSyDD1m9R18y7KklktowIlpof800b8rCDZ5w",
+  FIREBASE_AUTH_DOMAIN: "task-quest-b8aef.firebaseapp.com", 
+  FIREBASE_PROJECT_ID: "task-quest-b8aef",
+  FIREBASE_STORAGE_BUCKET: "task-quest-b8aef.firebasestorage.app",
+  FIREBASE_MESSAGING_SENDER_ID: "824530751369",
+  FIREBASE_APP_ID: "1:824530751369:web:0763380cd99faaba0b9b58",
+  FIREBASE_MEASUREMENT_ID: "G-6490XHZR5B"
+};
